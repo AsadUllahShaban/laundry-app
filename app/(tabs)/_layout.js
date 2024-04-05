f5c9090b68c523e76a551ce3920749c400df896e
Asad Ullah Shaban <asadullahshaban1997@gmai.com>
2024-04-05 17:21:48 +0500

    redux integrated

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -3,70 +3,74 @@ import { Feather } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { colors } from "../../assets/colors";
+import { Provider } from "react-redux";
+import store from "../../store";
 
 export default function layout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="home"
-        options={{
-          tabBarLabel: "Home",
-          tabBarLabelStyle: {
-            color: "black",
-          },
-          headerShown: false,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <Feather name="home" size={24} color={colors.primary} />
-            ) : (
-              <Feather name="home" size={24} color="black" />
-            ),
-        }}
-      />
-      <Tabs.Screen
-        name="basket"
-        options={{
-          tabBarLabel: "Basket",
-          tabBarLabelStyle: {
-            color: "black",
-          },
-          headerShown: false,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <Ionicons
-                name="basket-outline"
-                size={24}
-                color={colors.primary}
-              />
-            ) : (
-              <Ionicons name="basket-outline" size={24} color="black" />
-            ),
-        }}
-      />
-      <Tabs.Screen
-        name="orders"
-        options={{
-          tabBarLabel: "Orders",
-          tabBarLabelStyle: {
-            color: "black",
-          },
-          headerShown: false,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <MaterialCommunityIcons
-                name="account-details"
-                size={24}
-                color={colors.primary}
-              />
-            ) : (
-              <MaterialCommunityIcons
-                name="account-details"
-                size={24}
-                color="black"
-              />
-            ),
-        }}
-      />
-    </Tabs>
+    <Provider store={store}>
+      <Tabs>
+        <Tabs.Screen
+          name="home"
+          options={{
+            tabBarLabel: "Home",
+            tabBarLabelStyle: {
+              color: "black",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Feather name="home" size={24} color={colors.primary} />
+              ) : (
+                <Feather name="home" size={24} color="black" />
+              ),
+          }}
+        />
+        <Tabs.Screen
+          name="basket"
+          options={{
+            tabBarLabel: "Basket",
+            tabBarLabelStyle: {
+              color: "black",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons
+                  name="basket-outline"
+                  size={24}
+                  color={colors.primary}
+                />
+              ) : (
+                <Ionicons name="basket-outline" size={24} color="black" />
+              ),
+          }}
+        />
+        <Tabs.Screen
+          name="orders"
+          options={{
+            tabBarLabel: "Orders",
+            tabBarLabelStyle: {
+              color: "black",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <MaterialCommunityIcons
+                  name="account-details"
+                  size={24}
+                  color={colors.primary}
+                />
+              ) : (
+                <MaterialCommunityIcons
+                  name="account-details"
+                  size={24}
+                  color="black"
+                />
+              ),
+          }}
+        />
+      </Tabs>
+    </Provider>
   );
 }
